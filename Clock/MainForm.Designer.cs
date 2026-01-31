eeedfa0012e6952439abae5c26143891ee806449
irina <irina@COMPUTER>
2026-01-31 13:51:32 +0500

    FixedToolWindow

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -81,6 +81,7 @@
             this.Controls.Add(this.checkBoxShowWeekday);
             this.Controls.Add(this.checkBoxShowDate);
             this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
