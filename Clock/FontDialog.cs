a37e34de520631bbcbf7fd15c310440607dad405
DyadushkinaIS <irinadyadushkina@gmail.com>
2026-02-05 10:49:27 +0500

    fonts in progress

---
+++ Clock/FontDialog.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;    //Input/Output
+using System.Runtime.InteropServices;
+using System.Drawing.Text;
+
+namespace Clock
+{
+	public partial class FontDialog : Form
+	{
+		MainForm parent;
+		public Font Font { get; private set; }
+		public FontDialog(MainForm parent)
+		{
+			InitializeComponent();
+			this.StartPosition = FormStartPosition.Manual;
+			this.parent = parent;
+		}
+		void LoadFonts()
+		{
+			AllocConsole();
+			Console.WriteLine(Application.ExecutablePath);
+			//Directory.SetCurrentDirectory($"{Application.ExecutablePath}");
+			Directory.SetCurrentDirectory($"{Application.ExecutablePath}\\..\\..\\..\\Fonts");
+			//Console.WriteLine($"{Directory.GetParent(Application.StartupPath)?.Parent?.FullName}\\Fonts");
+			Console.WriteLine(Directory.GetCurrentDirectory());
+			//LoadFonts(Directory.GetCurrentDirectory(), "*.otf");
+			//LoadFonts(Directory.GetCurrentDirectory(), "*.ttf");
+			Traverse(Directory.GetCurrentDirectory());
+		}
+		void LoadFonts(string path, string extension)
+		{
+			string[] files = Directory.GetFiles(path, extension);
+			for (int i = 0; i < files.Length; i++)
+			{
+				//Console.WriteLine(files[i]);
+				files[i] = files[i].Split('\\').Last();
+			}
+			comboBoxFonts.Items.AddRange(files);
+		}
+		void Traverse(string path)
+		{
+			LoadFonts(path, "*.ttf");
+			LoadFonts(path, "*.otf");
+			string[] directories = Directory.GetDirectories(path);
+			if (directories.Length == 0) return;
+			for (int i = 0; i < directories.Length; i++)
+			{
+				Traverse(directories[i]);
+			}
+		}
+		[DllImport("kernel32.dll")]
+		public static extern void AllocConsole();
+		[DllImport("kernel32.dll")]
+		public static extern void FreeConsole();
+
+		private void FontDialog_Load(object sender, EventArgs e)
+		{
+			this.Location = new Point
+				(
+					this.parent.Location.X - this.Width/4,
+					this.parent.Location.Y + 100
+				);
+			LoadFonts();
+		}
+
+		private void buttonOK_Click(object sender, EventArgs e)
+		{
+			this.Font = labelExample.Font;
+		}
+		void ApplyFontExample()
+		{
+			PrivateFontCollection pfc = new PrivateFontCollection();
+			pfc.AddFontFile(comboBoxFonts.SelectedItem.ToString());
+			labelExample.Font = new Font(pfc.Families[0], (float)numericUpDownFontSize.Value);
+		}
+		private void comboBoxFonts_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			ApplyFontExample();
+		}
+
+		private void numericUpDownFontSize_ValueChanged(object sender, EventArgs e)
+		{
+			ApplyFontExample();
+		}
+	}
+}
